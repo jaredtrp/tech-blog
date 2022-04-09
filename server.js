c5678b5abc0503a30a2b58526e78db576a6e9e5c
Jared <jaredtrp@gmail.com>
2022-04-08 21:41:17 -0500

    corrected server.js imports

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-const helpers = require('./utils/helpers.js');
-const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers.js');
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SECRET,
